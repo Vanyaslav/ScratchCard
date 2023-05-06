b9268eb3966adccd1bed6a1c4f539f667aafcc49
Tomas Baculak <vanya.otvorij@gmail.com>
2023-05-06 22:05:52 +0200

    Initial Commit

---
+++ ScratchCard/ScratchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardApp.swift
+//  ScratchCard
+//
+//  Created by Tomas Baculák on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
